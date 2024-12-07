15f0058dc500b938118b94dcadb7fc1b7ceede22
Md Kutub Uddin <mdkutub150@gmail.com>
2024-12-07 16:19:46 +0600

    Update web.php

--- LaravelCrud/routes/web.php
+++ LaravelCrud/routes/web.php
@@ -4,13 +4,10 @@ use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
-// Home route, passing all posts to the welcome view
 Route::get('/', function () {
     return view('welcome', ['posts' => Post::all()]);
 })->name('home');
 
-// Route for creating a post
 Route::get('/create', [PostController::class, 'create']);
 
-// Route for storing a new post
 Route::post('/store', [PostController::class, 'ourfilestore'])->name('store');
