fb99594ecdaa0f7025c02fe6f64befad9cfe9f73
Md Kutub Uddin <mdkutub150@gmail.com>
2024-12-07 13:59:09 +0600

    Update welcome.blade.php

--- LaravelCrud/resources/views/welcome.blade.php
+++ LaravelCrud/resources/views/welcome.blade.php
@@ -1,12 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>Document</title>
+    <script
+        src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
+    <style type="text/tailwindcss">
+        @layer utilities {
+        .container {
+          @apply px-10 mx-auto;
+        }
+      }
+    </style>
+    <title>Laravel Greeting</title>
 </head>
-<body>
-    <h1 class="text-red-500">Hello Laravel</h1>
+
+<body class="bg-gray-50">
+    <div class="container">
+        <div class="flex justify-between">
+            <h1 class="text-red-500">Hello Laravel</h1>
+            <a href="/create">Add New Post</a>
+        </div>
+    </div>
 </body>
+
 </html>
